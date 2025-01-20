283de5953b8ce69d81bbf1b5552c3ddfe76bfad8
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2025-01-20 21:46:08 +0530

    Create Message.java

---
+++ LLD/PubSubSystem/Message.java
@@ -0,0 +1,12 @@
+// Message class to encapsulate the message details
+class Message {
+    private final String content;
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
